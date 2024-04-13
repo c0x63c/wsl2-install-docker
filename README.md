e5d96d4750efa5a64f7ba53c84b5b262d4feb7e8
c0x63c <c0x63c01@gmail.com>
2024-04-14 00:18:39 +0900

    Initail commits.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# wsl-install-docker
+### Description
+
+* WSL2にdocker(ce)及びdocker-composeをインストールします。
+
+### Prerequisites
+
+* Windows 11(64GB)
+* NVidia video card (RTX3060 12GB)
+* WSL2 (32GB and operation confirmed on Ubuntu 20.04)
+
+*上記以外の構成では未確認.
+
+## Usage
+
+```
+$ sudo apt install -y curl
+$ curl -s https://raw.githubusercontent.com/c0x63c/install-docker-wsl2/main/install-docker.sh | /bin/bash
+```
+
+## Version
+
+* 2024/04/13 新規登録
+
+## Acknowledgments
+
+* [ゲーミングPCのWindows環境セットアップ](https://zenn.dev/karaage0703/articles/211d89cc0a29a1)
+* [Install Docker Engine on Ubuntu](https://docs.docker.com/engine/install/ubuntu/)
+* [Linux に Docker を一発インストールするワンライナー](https://qiita.com/KEINOS/items/bdc9450c1a88c210aa88)
---
+++ install-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Reference from this site and sources.
+# https://docs.docker.com/engine/install/ubuntu/
+# https://qiita.com/KEINOS/items/bdc9450c1a88c210aa88
+# https://raw.githubusercontent.com/karaage0703/ubuntu-setup/master/install-docker.sh
+
+sudo apt update && sudo apt -y install ca-certificates aria2
+sudo mkdir -p /etc/apt/keyrings
+sudo aria2c https://download.docker.com/linux/ubuntu/gpg --allow-overwrite=true -d /etc/apt/keyrings -o docker.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo gpasswd -a $USER docker

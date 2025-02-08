2168ec3cbf5e088cdba7536042d73c83ccd57e34
c0x63c <c0x63c01@gmail.com>
2025-02-08 21:20:05 +0900

    fix: aria2cのサイレントモードに変更

--- install-docker.sh
+++ install-docker.sh
@@ -4,9 +4,9 @@
 # https://qiita.com/KEINOS/items/bdc9450c1a88c210aa88
 # https://raw.githubusercontent.com/karaage0703/ubuntu-setup/master/install-docker.sh
 
-sudo apt update && sudo apt -y install ca-certificates aria2 gnupg
+sudo apt update && sudo apt -y install ca-certificates aria2
 sudo mkdir -p /etc/apt/keyrings
-aria2c -q https://download.docker.com/linux/ubuntu/gpg -o | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo aria2c -q https://download.docker.com/linux/ubuntu/gpg --allow-overwrite=true -d /etc/apt/keyrings -o docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
